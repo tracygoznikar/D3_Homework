f806f2f0dc4dc2c56bf8c2b8ba51748529dab7ce
tracygoznikar <tracygoznikar@gmail.com>
2021-01-11 10:43:31 -0600

    adds axis functions

--- d3_data_journalism/assets/js/app.js
+++ d3_data_journalism/assets/js/app.js
@@ -9,7 +9,11 @@ d3.csv("data.csv").then(function(data){
     //create scale functions
     var xLinearScale = d3.scaleLinear()
     .domain([28, d3.max(data, d => d.age)])
+    .range([width, 0]);
+    var yLinearScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.smokes)])
     .range([height, 0]);
     //create axis functions
-    
+    var bottomAxis = d3.axisBottom(xLinearScale);
+    var leftAxis = d3.axisLeft(yLinearScale);
 })
